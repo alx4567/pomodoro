90067e538eee941c4dca3e658b0bfc7a72ffe573
Alex Fore <forealexander4@gmail.com>
2017-10-03 13:56:04 -0500

    reset session countdown

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var start_time = 25;
-    var seconds = 2;    
+    var seconds = start_time * 60;    
 
     var countDown = setInterval (function() {
         var minutes = parseInt(seconds / 60);
@@ -14,29 +14,36 @@ var start_time = 25;
         clock.textContent = minutes + ':' + seconds_left;
 
         if (seconds === 0) { 
-            var break_time = 1;
-            seconds = 10
-            var breakCountDown = setInterval (function() {
-                var minutes = parseInt(seconds / 60);
-                var seconds_left = seconds % 60;
-                
-                if(String(seconds_left).length < 2) {
-                    seconds_left = "0" + seconds_left;
-                }
-        
-        
-                var clock = document.getElementById("pom-time");
-                clock.textContent = minutes + ':' + seconds_left;
-        
-                if (seconds === 0) { 
-                    clearInterval()
-                  }
-                seconds -= 1;
-                
-                
-            }, 1000);
+            var clock = document.getElementById("pom-time");
+            clock.textContent = 'break';
+            clearInterval(countDown);
           }
         seconds -= 1;
         
         
     }, 1000);
+
+
+/*
+    var break_time = 1;
+    seconds = 10
+    var breakCountDown = setInterval (function() {
+        var minutes = parseInt(seconds / 60);
+        var seconds_left = seconds % 60;
+        
+        if(String(seconds_left).length < 2) {
+            seconds_left = "0" + seconds_left;
+        }
+
+
+        var clock = document.getElementById("pom-time");
+        clock.textContent = minutes + ':' + seconds_left;
+
+        if (seconds === 0) { 
+            clearInterval()
+          }
+        seconds -= 1;
+        ;
+        
+    }, 1000)
+    */
