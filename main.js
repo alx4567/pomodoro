afea22683a3e58bd8fa9c7e0d92ac008e10adc3a
Alex Fore <forealexander4@gmail.com>
2017-10-03 13:45:22 -0500

    Session timer added

---
+++ main.js
@@ -0,0 +1,42 @@
+var start_time = 25;
+    var seconds = 2;    
+
+    var countDown = setInterval (function() {
+        var minutes = parseInt(seconds / 60);
+        var seconds_left = seconds % 60;
+        
+        if(String(seconds_left).length < 2) {
+            seconds_left = "0" + seconds_left;
+        }
+
+
+        var clock = document.getElementById("pom-time");
+        clock.textContent = minutes + ':' + seconds_left;
+
+        if (seconds === 0) { 
+            var break_time = 1;
+            seconds = 10
+            var breakCountDown = setInterval (function() {
+                var minutes = parseInt(seconds / 60);
+                var seconds_left = seconds % 60;
+                
+                if(String(seconds_left).length < 2) {
+                    seconds_left = "0" + seconds_left;
+                }
+        
+        
+                var clock = document.getElementById("pom-time");
+                clock.textContent = minutes + ':' + seconds_left;
+        
+                if (seconds === 0) { 
+                    clearInterval()
+                  }
+                seconds -= 1;
+                
+                
+            }, 1000);
+          }
+        seconds -= 1;
+        
+        
+    }, 1000);
