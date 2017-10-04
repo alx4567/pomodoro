69e320be6fa5f176824b983e52e90bc3275234a7
Alex Fore <forealexander4@gmail.com>
2017-10-04 13:59:34 -0500

    cleaned up JS file

--- main.js
+++ main.js
@@ -8,8 +8,6 @@ $(document).ready(function(){
         var header = document.getElementById("header");
         var seconds_start = timeSeconds
 
-
-
     function countDown() {
         if (pause == 0) {
             var minute = parseInt(seconds_start / 60);
@@ -70,31 +68,3 @@ $(document).ready(function(){
         });
     });
 });
-
-
-
-/*
-    var break_time = 1;
-    seconds = 10
-    var breakCountDown = setInterval (function() {
-        var minutes = parseInt(seconds / 60);
-        var seconds_left = seconds % 60;
-        
-        if(String(seconds_left).length < 2) {
-            seconds_left = "0" + seconds_left;
-        }
-
-
-        var clock = document.getElementById("pom-time");
-        clock.textContent = minutes + ':' + seconds_left;
-
-        if (seconds === 0) { 
-            clearInterval()
-          }
-        seconds -= 1;
-        ;
-        
-    }, 1000)
-    */
-
-    
