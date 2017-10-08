8aac906aaa1a648a6eab4bde6b5c53cc51dc8b1c
Alex Fore <forealexander4@gmail.com>
2017-10-08 15:23:31 -0500

    finished css

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
     
     function pomodoro(timeSeconds) {
         var timer = document.getElementById("timer");
-        var header = document.getElementById("header");
         var seconds_start = timeSeconds
         var user_session = $('#userSession').html();
         var user_break = $('#userBreak').html();
@@ -24,16 +23,22 @@ $(document).ready(function(){
             timer.innerHTML = minute + ":" + seconds; //1
             
 
-            if (seconds_start <= -1) { 
+            if (seconds_start <= 0) { 
                 clearInterval(interval);
                 if (break_switch % 2 == 0) {
                     break_switch++
-                    header.innerHTML = 'Break';  		
                     pomodoro( parseInt(user_break) * 60 )
+                    $('header').removeClass("session").addClass('work');
+                    $('#display').removeClass("session").addClass('work');
+                    $('#topText').removeClass("current").addClass('next');
+                    $('#bottomText').removeClass("next").addClass('current');
                 } else {
                     break_switch--
-                    header.innerHTML = 'Session'
                     pomodoro( parseInt(user_session) * 60 )
+                    $('header').removeClass('work').addClass('session');
+                    $('#display').removeClass("work").addClass('session');
+                    $('#topText').removeClass("next").addClass('current');
+                    $('#bottomText').removeClass("current").addClass('next');
                 }
                                     
             }
@@ -43,19 +48,29 @@ $(document).ready(function(){
                 clearInterval(interval);
                 running = false
                 break_switch = 0
-                header.innerHTML = 'Session'
-                timer.innerHTML = $('#userSession').html() + ':00'   
-                    });
+                timer.innerHTML = $('#userSession').html() + ':00'  
+                if (break_switch % 2 == 0) {
+                    $('header').removeClass('work').addClass('session');
+                    $('#display').removeClass("work").addClass('session');
+                    $('#topText').removeClass("next").addClass('current');
+                    $('#bottomText').removeClass("current").addClass('next');
+                } 
+            });
 
                     //Refresh button
             $("#refresh").click(function(){
                 clearInterval(interval);
                 running = false
                 break_switch = 0
-                header.innerHTML = 'Session'
                 $('#userSession').html("25");
                 $('#userBreak').html("5");
                 timer.innerHTML = '25:00' 
+                if (break_switch % 2 == 0) {
+                    $('header').removeClass('work').addClass('session');
+                    $('#display').removeClass("work").addClass('session');
+                    $('#topText').removeClass("next").addClass('current');
+                    $('#bottomText').removeClass("current").addClass('next');
+                } 
             });
 
         }
@@ -71,7 +86,7 @@ $(document).ready(function(){
             if (running === false && pause === 0) {
                 running = true
                 var break_switch = 1
-                pomodoro( parseInt($('#userSession').html()) * 60 );
+                pomodoro(5);
             } else {
                 pause = 0;
                 running = true
@@ -92,7 +107,7 @@ $(document).ready(function(){
                 standart = parseInt(standart) - 1;
                 arrow.innerHTML = standart
 
-                $('#minutes').html( standart )
+                $('#timer').html( standart + ":00" )
             }
 
         });
@@ -104,7 +119,7 @@ $(document).ready(function(){
             if (standart < 99) {                
                 standart = parseInt(standart) + 1;
                 arrow.innerHTML = standart
-                $('#minutes').html( standart )
+                $('#timer').html( standart + ":00" )
             }
         });
 
